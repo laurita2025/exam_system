2bb107ce5e61a6365666f8903e421a12d2429445
Laura Cordero Becerra <laurita141985@gmail.com>
2026-06-15 19:12:54 -0400

    Update MongoDB Atlas connection URI

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://admin:Laura2026@cluster0.abcd123.mongodb.net/exam_system?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "exam_system")
 SECRET_KEY = os.environ.get("SECRET_KEY", "supersecret")
 EXAM_TIME = int(os.environ.get("EXAM_TIME", 1800))
