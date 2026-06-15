3a9e876f929973db726b1d5d2ee65dd95d4e1431
Laura Cordero Becerra <laurita141985@gmail.com>
2026-06-15 18:06:40 -0400

    Add deployment config for Render/Railway

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
+import os
 
-MONGO_URI = "mongodb://localhost:27017/"
-DB_NAME = "exam_system"
-SECRET_KEY = "supersecret"
-EXAM_TIME = 1800
-MAX_WARNINGS = 3
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
+DB_NAME = os.environ.get("DB_NAME", "exam_system")
+SECRET_KEY = os.environ.get("SECRET_KEY", "supersecret")
+EXAM_TIME = int(os.environ.get("EXAM_TIME", 1800))
+MAX_WARNINGS = int(os.environ.get("MAX_WARNINGS", 3))
